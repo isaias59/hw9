32cf46d71ee90d93a188b28ccca256dbbf382ee1
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-03-14 14:15:00 -0700

    Update map_array.hpp

--- src/map_array.hpp
+++ src/map_array.hpp
@@ -1,38 +1,63 @@
-#pragma once
-#include <iostream>
-#include <array>
+#ifndef MAP_ARRAY_HPP
+#define MAP_ARRAY_HPP
 
-using namespace std;
+#include <algorithm>
+#include <iterator>
+#include <ranges>
+#include <utility>
+#include <vector>
 
-template<typename K, typename V, size_t N>
+template <typename Key, typename Value>
 class MapArray {
-private:
-    std::array<std::pair<K, V>, N> elements;
-
 public:
-    void insert(const K& key, const V& value) {
-        for (auto& element : elements) {
-            if (element.first == key) {
-                std::cout << "Key already exists." << std::endl;
-                return;
-            }
+    class ArrayIterator {
+    public:
+        using iterator_category = std::random_access_iterator_tag;
+        using value_type = std::pair<Key, Value>;
+        using difference_type = std::ptrdiff_t;
+        using pointer = std::pair<Key, Value>*;
+        using reference = std::pair<Key, Value>&;
+
+        explicit ArrayIterator(pointer ptr = nullptr) : ptr(ptr) {}
+
+        ArrayIterator& operator++() {
+            ++ptr;
+            return *this;
         }
-        for (auto& element : elements) {
-            if (element.first == K()) {
-                element.first = key;
-                element.second = value;
-                return;
-            }
+
+        ArrayIterator operator++(int) {
+            ArrayIterator tmp = *this;
+            ++(*this);
+            return tmp;
         }
-        std::cout << "Map is full." << std::endl;
+
+        reference operator*() const { return *ptr; }
+        pointer operator->() const { return ptr; }
+        bool operator==(const ArrayIterator& other) const { return ptr == other.ptr; }
+        bool operator!=(const ArrayIterator& other) const { return !(*this == other); }
+
+    private:
+        pointer ptr;
+    };
+
+    MapArray() = default;
+
+    ArrayIterator begin() { return ArrayIterator(data.data()); }
+    ArrayIterator end() { return ArrayIterator(data.data() + data.size()); }
+
+    Value& operator[](const Key& key) {
+        auto it = std::find_if(data.begin(), data.end(), [&key](const auto& pair) { return pair.first == key; });
+        if (it == data.end()) {
+            data.push_back(std::make_pair(key, Value()));
+            return data.back().second;
+        }
+        else {
+            return it->second;
+        }
     }
 
-    void display() const {
-        for (const auto& element : elements) {
-            if (element.first != K()) {
-                std::cout << element.first << ": " << element.second << std::endl;
-            }
-        }
-    }
+private:
+    std::vector<std::pair<Key, Value>> data;
 };
 
+#endif

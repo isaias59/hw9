d73efb2167a6dae9f2823294983f0a58d6cf2ba7
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-03-14 14:15:24 -0700

    Update set_list.hpp

--- src/set_list.hpp
+++ src/set_list.hpp
@@ -1,26 +1,107 @@
-#pragma once
-#include <list>
+#ifndef SET_LIST_HPP
+#define SET_LIST_HPP
+
 #include <algorithm>
-#include <iostream>
+#include <functional>
+#include <iterator>
+#include <memory>
+#include <ranges>
 
-
-using namespace std;
-template<typename T>
+template <typename T>
 class SetList {
-private:
-    std::list<T> elements;
+    struct ListNode {
+        T data;
+        std::shared_ptr<ListNode> next;
+        ListNode(T data, std::shared_ptr<ListNode> next = nullptr) : data(data), next(next) {}
+    };
 
 public:
-    void insert(const T& element) {
-        if (std::find(elements.begin(), elements.end(), element) == elements.end()) {
-            elements.push_back(element);
+    class ListIterator {
+    public:
+        using iterator_category = std::forward_iterator_tag;
+        using value_type = T;
+        using difference_type = std::ptrdiff_t;
+        using pointer = T*;
+        using reference = T&;
+
+        explicit ListIterator(std::shared_ptr<ListNode> ptr = nullptr) : ptr(ptr) {}
+
+        ListIterator& operator++() {
+            if (ptr) ptr = ptr->next;
+            return *this;
         }
+
+        ListIterator operator++(int) {
+            ListIterator tmp = *this;
+            ++(*this);
+            return tmp;
+        }
+
+        reference operator*() const {
+            return ptr->data;
+        }
+
+        pointer operator->() const {
+            return &ptr->data;
+        }
+
+        bool operator==(const ListIterator& other) const {
+            return ptr == other.ptr;
+        }
+
+        bool operator!=(const ListIterator& other) const {
+            return !(*this == other);
+        }
+
+    private:
+        std::shared_ptr<ListNode> ptr;
+    };
+
+    SetList() = default;
+
+    template <std::ranges::input_range Rng>
+    explicit SetList(Rng&& rng) {
+        std::ranges::for_each(std::forward<Rng>(rng), [&](const T& value) { insert(value); });
     }
 
-    void display() const {
-        for (const auto& element : elements) {
-            std::cout << element << " ";
-        }
-        std::cout << std::endl;
+    ListIterator begin() {
+        return ListIterator(head);
     }
+
+    ListIterator end() {
+        return ListIterator(nullptr);
+    }
+
+    bool contains(const T& value) {
+        for (auto it = begin(); it != end(); ++it) {
+            if (*it == value) return true;
+        }
+        return false;
+    }
+
+    ListIterator insert(T value) {
+        std::shared_ptr<ListNode> node = std::make_shared<ListNode>(value);
+        if (!head || head->data > value) {
+            node->next = head;
+            head = node;
+        }
+        else {
+            auto prev = head;
+            auto curr = head->next;
+            while (curr && curr->data < value) {
+                prev = curr;
+                curr = curr->next;
+            }
+            if (!curr || curr->data > value) {
+                node->next = curr;
+                prev->next = node;
+            }
+        }
+        return ListIterator(node);
+    }
+
+private:
+    std::shared_ptr<ListNode> head = nullptr;
 };
+
+#endif

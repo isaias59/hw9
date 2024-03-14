570b499891e0b38151461eddadcee7c4d5540d3f
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-03-13 21:01:45 -0700

    Update set_list.hpp

--- src/set_list.hpp
+++ src/set_list.hpp
@@ -1,1 +1,26 @@
+#pragma once
+#include <list>
+#include <algorithm>
+#include <iostream>
 
+
+using namespace std;
+template<typename T>
+class SetList {
+private:
+    std::list<T> elements;
+
+public:
+    void insert(const T& element) {
+        if (std::find(elements.begin(), elements.end(), element) == elements.end()) {
+            elements.push_back(element);
+        }
+    }
+
+    void display() const {
+        for (const auto& element : elements) {
+            std::cout << element << " ";
+        }
+        std::cout << std::endl;
+    }
+};

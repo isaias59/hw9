5a89464fdb94aff5b1de77d933b3afa9e34e9aa6
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-03-14 14:13:42 -0700

    Update mapset_main.cpp

--- src/mapset_main.cpp
+++ src/mapset_main.cpp
@@ -1,23 +1,48 @@
-#include "set_list.hpp"
-#include "map_array.hpp"
+#include <algorithm>
+#include <fstream>
+#include <iterator>
+#include <ranges>
+#include <cctype> 
+
+#include "map_array.h"
+#include "set_list.h"
 
 using namespace std;
 
-int main() {
-    SetList<int> mySet;
-    mySet.insert(10);
-    mySet.insert(20);
-    mySet.insert(10);
-    mySet.display();
-
-    MapArray<std::string, int, 5> myMap;
-    myMap.insert("apple", 5);
-    myMap.insert("banana", 10);
-    myMap.insert("orange", 15);
-    myMap.insert("apple", 20); 
-    myMap.insert("grape", 25);
-    myMap.display();
-
-    return 0;
+string to_lowercase(const string& str) {
+    auto lower_view = str | views::transform([](unsigned char c) { return tolower(c); });
+    return { lower_view.begin(), lower_view.end() };
 }
 
+SetList<string> load_stopwords(istream& stopwords) {
+    return SetList<string>{ranges::istream_view<string>(stopwords) | views::transform(to_lowercase)};
+}
+
+MapArray<string, int> count_words(istream& document, SetList<string>& stopwords) {
+    auto words_view = ranges::istream_view<string>(document)
+        | views::transform(to_lowercase) | views::filter([&](const string& s) { return !stopwords.contains(s); });
+
+    MapArray<string, int> result;
+
+    for (const string& s : words_view) {
+        ++result[s];
+    }
+
+    return result;
+}
+
+void output_word_counts(MapArray<string, int>& word_counts, ostream& output) {
+    for (const auto& [word, count] : word_counts) {
+        output << word << ' ' << count << '\n';
+    }
+}
+
+int main() {
+    ifstream stopwords_file("stopwords.txt");
+    ifstream document("sample_doc.txt");
+    ofstream output("frequency.txt");
+
+    auto stopwords = load_stopwords(stopwords_file);
+    auto word_counts = count_words(document, stopwords);
+    output_word_counts(word_counts, output);
+}

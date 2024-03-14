80d938a92fbab14765876b0dfb0b8a95153df2e0
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-03-13 21:01:04 -0700

    Update mapset_main.cpp

--- src/mapset_main.cpp
+++ src/mapset_main.cpp
@@ -1,1 +1,23 @@
+#include "set_list.h"
+#include "map_array.h"
+
+using namespace std;
+
+int main() {
+    SetList<int> mySet;
+    mySet.insert(10);
+    mySet.insert(20);
+    mySet.insert(10);
+    mySet.display();
+
+    MapArray<std::string, int, 5> myMap;
+    myMap.insert("apple", 5);
+    myMap.insert("banana", 10);
+    myMap.insert("orange", 15);
+    myMap.insert("apple", 20); 
+    myMap.insert("grape", 25);
+    myMap.display();
+
+    return 0;
+}
 
